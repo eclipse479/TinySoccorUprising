75f0ca2a366a7f5dd9d0b288cab3d9aa8d51402b
aaron <aaronzstv@gmail.com>
2020-05-04 12:34:19 +1000

    prettyPlsWork

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class playerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody rb;
     void Start()
     {
-        //extraComment
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector3(-100, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(new Vector3(100, 0, 0) * Time.deltaTime);
+        }
     }
 }
