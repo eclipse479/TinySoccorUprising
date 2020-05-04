4b1e0b280eb6b3f24498a405bfd56e0dbfc6a5b8
Matthew <lynbrookmatt@outlook.com>
2020-05-04 16:17:43 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+
+    //should be set to the ball prefab
+    public GameObject spawnObject;
+
+    //if it is turned on
+    public bool isOn = true;
+
+    //the amount of balls currently in play
+    public int activeBalls = 0;
+
+    //iterator used in the ball timer
+    public float iterator = 0;
+
+    public float ballSpawnTimer = 5;
+
+    public float ballSpawnTimerMin = 2;
+
+    public float ballSpawnTimerMax = 6;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(isOn)
+        {
+            //if there are no balls, spawn one in
+            if(activeBalls == 0)
+            {
+                Instantiate(spawnObject, this.transform);
+                activeBalls++;
+            }
+
+            iterator += Time.deltaTime;
+
+            //once every few seconds spawn a ball
+            if(iterator > ballSpawnTimer)
+            {
+               //create new ball
+               Instantiate(spawnObject, this.transform);
+
+               //add a new ball to the count
+               activeBalls++;
+
+               //update the iterator
+               iterator -= ballSpawnTimer;
+
+               //make the new timer a random number in the range (var 1 is inclusive, var 2 is exclusive)
+               ballSpawnTimer = Random.Range(ballSpawnTimerMin, (ballSpawnTimerMax + 1));
+            }
+
+
+        }
+
+
+    }
+}
