0d5f669b21f281e54dcc9fe7c21d5f77a0f42889
aaron <aaronzstv@gmail.com>
2020-05-06 10:27:54 +1000

    added a music box prefab/script + updated player controls
    can now have 2 players and music can now exist

---
+++ Assets/Scripts/musicBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicBox : MonoBehaviour
+{
+    public bool firstScene = false;
+    // Start is called before the first frame update
+    private GameObject[] extra;
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        extra = GameObject.FindGameObjectsWithTag("musicBox");
+        if (extra.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
